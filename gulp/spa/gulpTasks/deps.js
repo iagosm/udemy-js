223a96a2787cb48893c0d13543c3ec61535bb32b
iagosm <iagosousa201486@gmail.com>
2022-12-21 09:58:39 -0300

    Finalizando modulo Gulp e o projeto SPA

--- gulp/spa/gulpTasks/deps.js
+++ gulp/spa/gulpTasks/deps.js
@@ -1,13 +1,19 @@
 //dependencias externas, tipo bootstrap 
 const gulp = require('gulp')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
 //apenas um css de dependencia
 
-function depsCSS(cb){
-    return cb()
+function depsCSS(){
+    return gulp.src('node_modules/font-awesome/css/font-awesome.css')
+    .pipe(uglifycss({"uglyComments": false}))
+    .pipe(concat('deps.min.css'))
+    .pipe(gulp.dest('build/assets/css'))
 }
 
-function depsFonts(cb){
-    return cb()
+function depsFonts(){
+    return gulp.src('node_modules/font-awesome/fonts/*.*')
+    .pipe(gulp.dest('build/assets/fonts'))
 }
 
 module.exports = {
--- gulp/spa/gulpfile.js
+++ gulp/spa/gulpfile.js
@@ -4,3 +4,12 @@ const gulp = require('gulp')
 const { appHTML,appCSS,appJS,appIMG} = require('./gulpTasks/app')
 const { depsCSS,depsFonts} = require('./gulpTasks/deps')
 const { monitorarArquivos,servidor} = require('./gulpTasks/servidor')
+
+module.exports.default = series(
+    parallel(
+        series(appHTML, appCSS, appJS, appIMG),
+        series(depsCSS, depsFonts)
+    ),
+    servidor,
+    monitorarArquivos
+)
